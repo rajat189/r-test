834adad8630963ab6e012cd4bd5c17f2b40b2999
Shailendra Gupta <guptas199614@gmail.com>
2018-10-31 10:37:31 +0530

    Test 1

---
+++ will.r
@@ -0,0 +1,19 @@
+library(RPostgreSQL);
+library(dplyr);
+library(e1071);
+
+USERNAME = Sys.getenv('REDSHIFT_USERNAME')
+PASSWORD = Sys.getenv('REDSHIFT_PASSWORD')
+HOST = Sys.getenv('REDSHIFT_HOST')
+print(USERNAME)
+print(PASSWORD)
+print(HOST)
+
+con <- dbConnect(dbDriver("PostgreSQL"),
+                  dbname = "elchipo",
+                  host = HOST,
+                  port = 5439,
+                  user = USERNAME,
+                  password = PASSWORD
+)
+dbGetQuery(con,"INSERT INTO publish.testqa (id, name, email) VALUES (1,'R1','R1@mail.com');")
